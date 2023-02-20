a25bdb19450247c17e875a6c6c1158b98e2c4af7
Mert Alp Taytak <mertalp.taytak@canonical.com>
2023-02-21 01:19:16 +0300

    Fully implemented second algorithm with optimized minimum union combiner.

--- tamus.py
+++ tamus.py
@@ -1,8 +1,11 @@
+from copy import deepcopy
+from functools import reduce
 import sys
 import tempfile
 import time
 import argparse
 import os
+from typing import Optional
 
 from explorer import Explorer
 from uppaalHelpers import pyuppaal
@@ -232,6 +235,7 @@ class Tamus:
                        False for _ in range(self.dimension)], depth=0)
         elif t == "nvac":
             self.nonvacuity()
+            return
         else:
             assert False
         print("MSRs:", len(self.msres))
@@ -664,7 +668,7 @@ class Tamus:
         print("Elapsed time in seconds after LP:",
               (time.process_time() - self.start_time))
 
-    def nonvacuity(self):
+    def nonvacuity(self, find_all=False, use_treewalker=False, use_optimizer=True):
         """
         Given N timed automata, ensure each one can reach its accepting state without making another TA go into error.
 
@@ -681,11 +685,60 @@ class Tamus:
                     system Template1, Template2;
                 </system>
             In query file, we do this:
-                E<> Template1.accept
-                E<> Template2.accept
+                E<> (Template1.accept && !Template2.error)
+                E<> (Template2.accept && !Template1.error)
         """
+        def minimum_union_combiner(
+                msr_sets: list[set[str]], 
+                idx: int, union: set[str], 
+                card: Optional[int] = None,
+        ) -> tuple[set[str], int]:
+            """Find the minimum union of constraints to remove by picking
+            a constraint set for each automaton in the system from the
+            input list of MSR sets.
+            """
+            if idx == len(msr_sets):
+                return union, len(union)
+            
+            min_union, min_card = None, card
+            for msr_set in msr_sets[idx]:
+                tmp = union.union(msr_set)
+                if min_card is not None and len(tmp) >= min_card:
+                    continue
+                tmp_union, tmp_card = minimum_union_combiner(msr_sets, idx+1, tmp, min_card)
+                if min_card is None or tmp_card < min_card:
+                    min_union, min_card = tmp_union, tmp_card
+            
+            return min_union, min_card
+
+        def combiner_optimizer(msr_sets: list[list[set[str]]]) -> tuple[list[list[set[str]]], set[str]]:
+            """Extract common constraints and sorts by set size for average case optimization."""
+            msr_sets = deepcopy(msr_sets)
+            
+            # First sort pass
+            for idx in range(len(msr_sets)):
+                msr_sets[idx].sort(key=lambda x: len(x))
+
+            # Extraction pass
+            extracts = []
+            for curr_sets in msr_sets:
+                extract = reduce(lambda x, y: x.intersection(y), curr_sets, curr_sets[0])
+                extracts.append(extract)
+
+            # Removal pass
+            extract = reduce(lambda x, y: x.union(y), extracts, set())
+            for idx, curr_sets in enumerate(msr_sets):
+                for jdx, curr_set in enumerate(curr_sets):
+                    curr_sets[jdx] = curr_set.difference(extract)
+
+            # Second sort pass
+            for idx in range(len(msr_sets)):
+                msr_sets[idx].sort(key=lambda x: len(x))
+
+            return msr_sets, extract
+
         def nvac_helper_1(queries, templates, curr_idx, solutions, base_dir='.', find_all=True):
-            """Runs a recursive search on the TAs."""
+            """Run a recursive search on the TAs."""
             # No more templates
             if curr_idx == len(templates):
                 solutions.append(templates)
@@ -723,9 +776,9 @@ class Tamus:
                 return
             raise Exception('Non-vacuity is impossible.')
 
-        def nvac_helper_2(queries, templates, base_dir='.', findAll=True):
-            """Runs an iterative search on the TAs."""
-            candidates = []
+        def nvac_helper_2(queries, templates, find_all=True):
+            """Run an iterative search on the TAs."""
+            msr_sets = []
             for idx, _ in enumerate(templates):
                 print(f'Curr Idx: {idx} out of {len(templates) - 1}')
                 curr_query_path = queries[idx]
@@ -743,17 +796,44 @@ class Tamus:
                 curr_tamus.minimumMSR(False)
                 print('Getting MSRs')
                 msres, _, _ = curr_tamus.get_MSRes()
+                msr_set = [set(msr) for msr in msres]
+                msr_sets.append(msr_set)
+            
+            if not find_all:
+                # Calculate the minimal overall reduction
+                if use_optimizer:
+                    sets, extract = combiner_optimizer(msr_sets)
+                else:
+                    sets, extract = msr_sets, set()
+                solution, _ = minimum_union_combiner(sets, 0, set())
+                solution = list(solution.union(extract))
+                msres = [[solution]] * len(templates)
+            else:
+                msres = [[list(msr) for msr in msr_set] for msr_set in msr_sets]
+
+            print('Generating templates')
+            candidates = []
+            for idx, curr_msres in enumerate(msres):
+                print(f'Curr Idx: {idx} out of {len(templates) - 1}')
+                curr_query_path = queries[idx]
+                # We will trick Tamus into thinking we called a subtemplate with the 'amsr' task
+                # We will be calling the generating function manually, so not sure how much of this is necessary
+                args_var = vars(self.args)
+                args_var['task'] = 'amsr'
+                args_copy = argparse.Namespace(**args_var)
+                curr_tamus = Tamus(self.model_file, curr_query_path, "All", args_copy)
+                curr_tamus.timelimit = args_copy.msr_timelimit if args_copy.msr_timelimit != None else 1000000
+                curr_tamus.verbosity = args_copy.verbose if args_copy.verbose != None else 0
+                curr_tamus.task = args_copy.task
+                curr_tamus.usePathAnalysis = args_copy.path_analysis
+                curr_tamus.useMultiplePathCores = args_copy.multiple_path_cores
                 curr_templates = []
-                print('Processing MSRs')
-                for msr in msres:
+                print('Processing MSRs into templates')
+                for msr in curr_msres:
                     msr_templates = curr_tamus.TA.generate_relaxed_templates(msr)
                     curr_templates.append(msr_templates)
-                candidates.append(curr_templates)
-            # Calculate the minimal overall reduction
-            # We will pick the first ones for now
-            solution = [ls[0] for ls in candidates]
-            return solution
-            
+                candidates.extend(curr_templates)
+            return candidates
 
         start_time = time.process_time()
         with tempfile.TemporaryDirectory() as tempdir:
@@ -766,9 +846,11 @@ class Tamus:
                     queries.append(query_path)
                     with open(query_path, mode='w') as f:
                         f.write(line)
-            results = []
-            # nvac_helper_1(queries, templates, 0, results, tempdir, False)
-            results = nvac_helper_2(queries, templates)
+            if use_treewalker:
+                results = []
+                nvac_helper_1(queries, templates, 0, results, tempdir, find_all)
+            else:
+                results = nvac_helper_2(queries, templates, find_all)
             for idx, res in enumerate(results):
                 res_nta = pyuppaal.NTA(self.model.declaration, self.model.system, res)
                 res_model_path = ta_helper.set_templates_and_save(
